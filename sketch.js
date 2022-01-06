cbb5aedebd8ec4b0a9a8dfc952576ffd8a92beca
Hubert Ekvall <hubertekvall@gmail.com>
2022-01-06 02:09:06 +0100

    breakthrough

--- sketch.js
+++ sketch.js
@@ -1,23 +1,65 @@
 
 
-let color = "RED";
 let capture;
+let img;
+let palette;
+
+function getPalette() {
+    img.loadPixels();
+    let quantizeArray = [];
+    let pixels = img.pixels;
+
+    for (let i = 0; i < pixels.length; i += 4) {
+        let color = [
+            pixels[i],
+            pixels[i+1],
+            pixels[i+2]
+        ];
+        quantizeArray.push(color);
+      
+    }
+
+
+    let maximumColorCount = 10;
+    let colorMap = MMCQ.quantize(quantizeArray, maximumColorCount);
+    palette = colorMap.palette();
+    console.log(palette);
+}
+
+
+function preload() {
+    img = loadImage('test.png');
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     noStroke();
     background(0);
 
-    capture = createCapture(VIDEO);
-    capture.size(windowWidth, windowHeight);
-   
+
+    getPalette();
+    // capture = createCapture(VIDEO);
+    // capture.size(windowWidth, windowHeight);
+
+
 }
+
+
+function drawPalette(){
+    noStroke();
+    for(let i = 0; i < palette.length; i++){
+        fill(palette[i][0], palette[i][1], palette[i][2]);
+        rect(windowWidth/palette.length * i, 0, windowWidth/palette.length * i, windowHeight);
+    }
+}
+
 
 function draw() {
     background(255);
-    image(capture, 0,0, windowWidth, windowHeight);
 
+    drawPalette();
 }
+
 
 // function mousePressed() {
 //     color = "BLUE";
