a10404b08915deb1033c2c4713eb55448d121f39
Hubert Ekvall <hubertekvall@gmail.com>
2022-01-07 16:44:24 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,94 +5,89 @@ let palette;
 let state = "PHOTO";
 
 
-function getPalette(img) {
-    img.loadPixels();
-    let quantizeArray = [];
-    let pixels = img.pixels;
+async function getPalette() {
 
-    for (let i = 0; i < pixels.length; i += 4) {
+    capture.loadPixels();
+    let quantizeArray = [];
+    let cpixels = capture.pixels;
+
+    for (let i = 0; i < cpixels.length; i += 4) {
         let color = [
-            pixels[i],
-            pixels[i+1],
-            pixels[i+2]
+            cpixels[i],
+            cpixels[i+1],
+            cpixels[i+2]
         ];
         quantizeArray.push(color);
       
     }
 
 
-    let maximumColorCount = 10;
+    let maximumColorCount = 5;
     let colorMap = MMCQ.quantize(quantizeArray, maximumColorCount);
     palette = colorMap.palette();
     console.log(palette);
 }
 
 
-// function preload() {
-//     img = loadImage('test.png');
-// }
  
 function setup() {
     createCanvas(windowWidth, windowHeight);
     noStroke();
     background(0);
-
-
-
-    capture = createCapture(VIDEO);
-    capture.size(windowWidth, windowHeight);
+    createVideoCapture();
     capture.hide();
+    // getPalette();
+   
 }
+
+function createVideoCapture(){
+    let constraints = {
+        video: {
+          mandatory: {
+            minWidth: 1280,
+            minHeight: 720
+          },
+          optional: [{ maxFrameRate: 10 }]
+        }
+      };
+      capture = createCapture(constraints, function(stream) {
+        console.log(stream);
+      });
+    
+}
+
 
 
 function drawPalette(){
     noStroke();
     for(let i = 0; i < palette.length; i++){
         fill(palette[i][0], palette[i][1], palette[i][2]);
-        rect(windowWidth/palette.length * i, 0, windowWidth/palette.length * i, windowHeight);
+        rect(width/palette.length * i, 0, width/palette.length, height);
     }
 }
 
 
 function draw() {
     background(255);
-    switch(state){
-        case "PHOTO":
-            image(capture, 0, 0, windowWidth, windowHeight );
-        break;
+    // image(capture, 0,0, capture.width / 2, capture.height/2);
+    
+    if(palette !== undefined) drawPalette();
 
-        case "PALETTE":
-            drawPalette();
-        break;
 
-    }
+    fill('red');
+    textAlign(CENTER,CENTER);
+    textSize(50);
+    // Convert the acceleration into integer when
+    // Device is moved along y-axis.
+    text(int(pAccelerationY),windowWidth/2,windowHeight/2);
+    text(int(pAccelerationX),windowWidth/2,windowHeight/2 + 50);
+    text(int(pAccelerationZ),windowWidth/2,windowHeight/2 + 100);
 }
 
 
-function mouseReleased() {
-    switch(state){
-        case "PHOTO":
-            getPalette(capture);
-            state = "PALETTE"
-        break;
-
-        case "PALETTE":
-            state = "PHOTO"
-            break;
-    }
-}
 
 function touchEnded() {
-    switch(state){
-        case "PHOTO":
-            getPalette(capture);
-            state = "PALETTE"
-        break;
-
-        case "PALETTE":
-            state = "PHOTO"
-            break;
-    }
+    getPalette();
 }
 
 
